255732eb053fbf652f85a18307959a2d0fe8a4a1
Condis <ilya_vais@mail.ru>
2018-09-24 01:22:01 +0700

    Create lesson

---
+++ Assets/Scripts/LocalizationClient.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizationClient : MonoBehaviour
+{
+    public string ru;
+    public string en;
+
+    private Text textComponent;
+
+    private void Start()
+    {
+        textComponent = GetComponent<Text>();
+
+        SetLanguage(LocalizationManager.GetLanguage());
+
+        LocalizationManager.ChangeLanguage.AddListener(SetLanguage);
+    }
+
+    private void SetLanguage(LocalizationManager.EnLanguage _language)
+    {
+        textComponent.text = _language == LocalizationManager.EnLanguage.RU ? ru : en;
+    }
+}
---
+++ Assets/Scripts/LocalizationManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class LocalizationManager : MonoBehaviour
+{
+    public enum EnLanguage
+    {
+        RU,
+        EN
+    }
+
+    [SerializeField]
+    private EnLanguage laguage;
+
+    [Space]
+    public Button selectRu;
+    public Button selectEn;
+
+    public class EnLanguageEvent : UnityEvent<EnLanguage> { }
+
+    public static EnLanguageEvent ChangeLanguage { get; set; } = new EnLanguageEvent();
+
+    private static LocalizationManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    private void Start()
+    {
+        selectRu.onClick.AddListener(() => SelectLanguage(EnLanguage.RU));
+        selectEn.onClick.AddListener(() => SelectLanguage(EnLanguage.EN));
+    }
+
+    private void SelectLanguage(EnLanguage _language)
+    {
+        laguage = _language;
+        ChangeLanguage.Invoke(laguage);
+    }
+
+    public static EnLanguage GetLanguage()
+    {
+        return instance.laguage;
+    }
+}
